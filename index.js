de15501ee59a66057590bf6f4bff53c439e7f27b
bideex24 <bideex24@gmail.com>
2025-02-26 04:21:53 +0600

    agent cash in

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = 3000;
 // middleware
 app.use(express.json());
@@ -20,6 +21,11 @@ async function run() {
   try {
     await client.connect();
     const userCollection = client.db("trustpaydb").collection("users");
+    const agentTranjectionCollection = client
+      .db("trustpaydb")
+      .collection("agentTranjection");
+    // tranjectionId create
+    const tran_Id = new ObjectId().toString();
     // jwt create api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
@@ -53,35 +59,22 @@ async function run() {
       if (exist) {
         return res.send({ message: "User Already Exist!" });
       }
+      // Hash the PIN before storing it
+      const salt = await bcrypt.genSalt(10);
+      const hashedPin = await bcrypt.hash(user.pin, salt);
 
-      if (user.role == "agent") {
-        const userInfo = {
-          name: user.name,
-          phone: user.phone,
-          email: user.email,
-          role: user.role,
-          pin: user.pin,
-          nid: user.nid,
-          amount: "00",
-          status: false,
-        };
-        const result = await userCollection.insertOne(userInfo);
-        res.send(result);
-        console.log(user);
-      } else {
-        const userInfo = {
-          name: user.name,
-          phone: user.phone,
-          email: user.email,
-          role: user.role,
-          pin: user.pin,
-          nid: user.nid,
-          amount: "40",
-          status: false,
-        };
-        const result = await userCollection.insertOne(userInfo);
-        res.send(result);
-      }
+      const userInfo = {
+        name: user.name,
+        phone: user.phone,
+        email: user.email,
+        role: user.role,
+        pin: hashedPin,
+        nid: user.nid,
+        amount: user.role === "agent" ? "00" : "40",
+        status: false,
+      };
+      const result = await userCollection.insertOne(userInfo);
+      res.send(result);
     });
     app.post("/user-login", async (req, res) => {
       const user = req.body;
@@ -107,10 +100,95 @@ async function run() {
           query.push({ $or: [{ email: user.email }, { phone: user.email }] });
         }
       }
-
       const exist = await userCollection.findOne({ $or: query });
-      // console.log(user);
       res.send(exist);
+    });
+    app.get("/all-users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.patch("/user/:id", verifyToken, async (req, res) => {
+      const user = req.body;
+      const phone = req.params.id;
+      const filter = { phone: phone };
+      const exist = await userCollection.findOne(filter);
+      const updatedDoc = {
+        $set: {
+          name: user.name,
+          phone: user.phone,
+          email: user.email,
+          role: "agent",
+          pin: user.pin,
+          nid: user.nid,
+          amount: Number(100000),
+          status: true,
+        },
+      };
+      const result = await userCollection.updateOne(exist, updatedDoc);
+      res.send(result);
+    });
+    // agent api
+    app.patch("/agent-user/:id", verifyToken, async (req, res) => {
+      const cashIn = req.body;
+      const userPhone = req.params.id;
+      const email = { email: cashIn.email };
+      const phone = { phone: userPhone };
+      const agentUser = await userCollection.findOne(email);
+      const user = await userCollection.findOne(phone);
+      const userPreviousAmount = parseInt(user.amount);
+      const userCashIn = parseInt(cashIn?.amount);
+      const agentPreviousAmount = parseInt(agentUser?.amount);
+      const agentRemainingAmount = agentPreviousAmount - userCashIn;
+      const userTotalCashIn = userPreviousAmount + userCashIn;
+      const updatedDocAgent = {
+        $set: {
+          name: agentUser.name,
+          phone: agentUser.phone,
+          email: agentUser.email,
+          role: agentUser.role,
+          pin: agentUser.pin,
+          nid: agentUser.nid,
+          amount: agentRemainingAmount,
+          status: agentUser.status,
+          tranjectionId: tran_Id,
+        },
+      };
+      const updatedDocUser = {
+        $set: {
+          name: user.name,
+          phone: user.phone,
+          email: user.email,
+          role: user.role,
+          pin: user.pin,
+          nid: user.nid,
+          amount: userTotalCashIn,
+          status: true,
+          tranjectionId: tran_Id,
+        },
+      };
+      const updateAgentUser = await userCollection.updateOne(
+        agentUser,
+        updatedDocAgent
+      );
+      const updateUser = await userCollection.updateOne(user, updatedDocUser);
+      const agentTranjection = {
+        name: user.name,
+        phone: user.phone,
+        email: agentUser.email,
+        role: agentUser.role,
+        pin: agentUser.pin,
+        nid: agentUser.nid,
+        amount: userCashIn,
+        status: agentUser.status,
+        tranjectionId: tran_Id,
+      };
+      await agentTranjectionCollection.insertOne(agentTranjection);
+      res.send(updateAgentUser);
+    });
+    app.get("/agent-user", verifyToken, async (req, res) => {
+      const email = req.query;
+      const result = await agentTranjectionCollection.find(email).toArray();
+      res.send(result);
     });
   } finally {
     // Ensures that the client will close when you finish/error
